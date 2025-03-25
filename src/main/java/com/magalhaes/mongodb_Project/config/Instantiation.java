fe37afec38ca94b1a3f5ef6080de756e0355f289
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-03-25 11:18:47 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/magalhaes/mongodb_Project/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.magalhaes.mongodb_Project.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.magalhaes.mongodb_Project.domain.User;
+import com.magalhaes.mongodb_Project.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
