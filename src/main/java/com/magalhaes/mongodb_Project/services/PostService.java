5c8ca7f1a279c5f00f4b39f70f37ac5dac8bf6b1
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-03-26 11:04:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/magalhaes/mongodb_Project/services/PostService.java
@@ -0,0 +1,24 @@
+package com.magalhaes.mongodb_Project.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.magalhaes.mongodb_Project.domain.Post;
+import com.magalhaes.mongodb_Project.repository.PostRepository;
+import com.magalhaes.mongodb_Project.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+
+}
