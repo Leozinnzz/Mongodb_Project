b79e8b60180ee5082b1d7bf25a7ebf683f1db763
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-03-27 09:56:54 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/magalhaes/mongodb_Project/config/Instantiation.java
+++ src/main/java/com/magalhaes/mongodb_Project/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.magalhaes.mongodb_Project.domain.Post;
 import com.magalhaes.mongodb_Project.domain.User;
 import com.magalhaes.mongodb_Project.dto.AuthorDTO;
+import com.magalhaes.mongodb_Project.dto.CommentDTO;
 import com.magalhaes.mongodb_Project.repository.PostRepository;
 import com.magalhaes.mongodb_Project.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("26/05/2025"), "Partiu viagem", "Vou viajar para a Bahia. Flw!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("28/03/2025"), "Bom dia", "Acordei agitado hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("25/03/2025"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("26/03/2025"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("27/03/2025"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
